46b32b1554d8dac0274695a3cb50ca2f03a2a206
manhood <manhood@pop-os.localdomain>
2023-06-13 10:06:00 +0300

    icons map moved to icon component

--- src/components/AppIcon/index.tsx
+++ src/components/AppIcon/index.tsx
@@ -1,4 +1,14 @@
-import { iconsMap } from "../../constants/icons";
+import MenuIcon from "@mui/icons-material/Menu";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ThemeIcon from "@mui/icons-material/Brightness6";
+
+const iconsMap = {
+  menu: <MenuIcon />,
+  linkedin: <LinkedInIcon />,
+  github: <GitHubIcon />,
+  theme: <ThemeIcon />,
+};
 
 interface Props {
   icon: keyof typeof iconsMap;
