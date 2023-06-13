e472ffc873d1b94be7a80bdbbb3edc89318da876
manhood <manhood@pop-os.localdomain>
2023-06-13 22:31:08 +0300

    redux middleware typing tested

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Middleware, combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { settingsReducer } from "./settings/slice";
 
+const loggerMiddleware: Middleware<object, RootState> = (store) => (next) => (action) => {
+  console.log("dispatching", action);
+  const result = next(action);
+  console.log("next state", store.getState());
+  return result;
+};
+
+const rootReducer = combineReducers({
+  settings: settingsReducer,
+});
 export const store = configureStore({
-  reducer: {
-    settings: settingsReducer,
-  },
+  reducer: rootReducer,
+  middleware: (gDM) => gDM().concat(loggerMiddleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
