1093b7033dfba3248469653a8032a87f832de47b
manhood <manhood@pop-os.localdomain>
2023-06-16 09:17:58 +0300

    home page text fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,11 @@ const HomePage = () => {
         Additionally, I possess a sufficient understanding of .NET Core and Express.js, allowing me
         to leverage their power in developing robust web applications. Inspired by the elegance of
         minimalism, I strive to create digital experiences that are both visually captivating and
-        functionally seamless. With every line of code I write, I aim to transcend the realm of mere
-        functionality and elevate it to a realm where beauty and user experience converge. In this
-        portfolio, you'll find a curated collection of my most notable projects. Driven by an
-        insatiable thirst for knowledge and a relentless pursuit of new challenges, I am eager to
-        embark on future endeavors that push the boundaries of what's possible. By leveraging the
-        latest tools and frameworks, I strive to deliver innovative and cutting-edge solutions that
-        drive your business forward.
+        functionally seamless. In this portfolio, you'll find a curated collection of my most
+        notable projects. Driven by an insatiable thirst for knowledge and a relentless pursuit of
+        new challenges, I am eager to embark on future endeavors that push the boundaries of what's
+        possible. By leveraging the latest tools and frameworks, I strive to deliver innovative and
+        cutting-edge solutions that drive your business forward.
       </Typography>
       <Typography>
         Collaboration is at the heart of my work philosophy. I firmly believe that the best results
