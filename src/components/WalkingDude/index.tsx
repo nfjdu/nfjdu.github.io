1219cdc527840483f848efa7f430cbf7aae1515d
manhood <manhood@pop-os.localdomain>
2023-07-02 20:43:11 +0300

    dude tap draft

--- src/components/WalkingDude/index.tsx
+++ src/components/WalkingDude/index.tsx
@@ -1,49 +1,64 @@
 import { useEffect, useState } from "react";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useAnimationFrame } from "framer-motion";
 
-const dudeWidth = 28;
-const dudeHeight = 49;
-const dudeAnimationFramesCount = 9;
+const walkingDudeWidth = 28;
+const walkingDudeHeight = 49;
+const walkingDudeAnimationFramesCount = 9;
+const idleDudeWidth = 64;
+const idleDudeHeight = 49;
+const idleDudeAnimationFramesCount = 5;
 
 const WalkingDude = () => {
+  const [currentAnimation, setCurrentAnimation] = useState<"walking" | "idle">("idle");
   const [currentWalkingFrame, setCurrentWalkingFrame] = useState(0);
+  const [currentIdleFrame, setCurrentIdleFrame] = useState(0);
+  const isWalking = currentAnimation === "walking";
+  const [walkingDirection, setWalkingDirection] = useState<"left" | "right">("left");
 
   useEffect(() => {
     setTimeout(() => {
-      if (currentWalkingFrame < dudeAnimationFramesCount - 1) {
-        setCurrentWalkingFrame(currentWalkingFrame + 1);
+      const currentFrame = isWalking ? currentWalkingFrame : currentIdleFrame;
+      const maxFramesCount = isWalking
+        ? walkingDudeAnimationFramesCount
+        : idleDudeAnimationFramesCount;
+      const setFn = isWalking ? setCurrentWalkingFrame : setCurrentIdleFrame;
+      if (currentFrame < maxFramesCount - 1) {
+        setFn(currentFrame + 1);
       } else {
-        setCurrentWalkingFrame(0);
+        setFn(0);
       }
     }, 150);
-  }, [currentWalkingFrame]);
+  }, [currentWalkingFrame, currentIdleFrame, isWalking]);
 
   const right = useMotionValue(0);
-  const scaleX = useMotionValue(1);
 
-  const handleUpdate = () => {
-    if (right.get() < right.getPrevious()) {
-      scaleX.set(1);
-    } else {
-      scaleX.set(-1);
+  useAnimationFrame(() => {
+    const val = right.get();
+    const newVal = val + (walkingDirection === "left" ? 1 : -1);
+    if (isWalking) {
+      right.set(newVal);
     }
-  };
+    if (val <= 0) {
+      setWalkingDirection("left");
+    } else if (val >= 200) {
+      setWalkingDirection("right");
+    }
+  });
 
   return (
     <motion.div
+      onClick={() => setCurrentAnimation(isWalking ? "idle" : "walking")}
       style={{
         cursor: "pointer",
-        width: `${dudeWidth}px`,
-        minHeight: `${dudeHeight}px`,
-        background: `url(DudeWalking.png) -${currentWalkingFrame * dudeWidth}px 0`,
+        width: `${walkingDudeWidth}px`,
+        minHeight: `${isWalking ? walkingDudeHeight : idleDudeHeight}px`,
+        background: isWalking
+          ? `url(DudeWalking.png) -${currentWalkingFrame * walkingDudeWidth}px 0px`
+          : `url(DudeLookingUp.png) -${currentIdleFrame * idleDudeWidth}px 0px`,
         position: "fixed",
         bottom: 0,
         right: right,
-        scaleX,
-      }}
-      animate={{ right: [0, 200, 0] }}
-      transition={{
-        right: { duration: 14, ease: "linear", repeat: Infinity, onUpdate: handleUpdate },
+        scaleX: walkingDirection === "left" ? -1 : 1,
       }}
     />
   );
