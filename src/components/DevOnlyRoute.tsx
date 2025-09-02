d4d6456165cbd77b70624ca1475c6e9d44558c3e
nfjdu <mosienko20133@gmail.com>
2025-09-02 16:08:50 +0300

    fix: cond fix back

--- src/components/DevOnlyRoute.tsx
+++ src/components/DevOnlyRoute.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function DevOnly({ element }: { element: React.ReactNode }) {
-  return import.meta.env.MODE !== "development" ? element : <Navigate to='/home' />;
+  return import.meta.env.MODE === "development" ? element : <Navigate to='/home' />;
 }
